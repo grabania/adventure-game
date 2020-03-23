95157fe324bf01dfd5c0af14d02c08f835b70f4d
grabania <grabania.work@gmail.com>
2020-03-23 15:36:54 +0000

    Update intro function

--- adventuregame.py
+++ adventuregame.py
@@ -7,8 +7,14 @@ def print_pause(message_to_print):
 
 
 def intro():
-    print_pause("You find yourself in a dark dungeon.")
-    print_pause("In front of you are two passageways.")
+    print_pause("You find yourself standing in an open field"
+                " filled with grass and yellow wildflowers.")
+    print_pause("Rumor has it that a gorgon is somewhere around"
+                " here, and has been terrifying the nearby village.")
+    print_pause("In front of you in the house.")
+    print_pause("To your right is a dark cave.")
+    print_pause("In your hand you hold your trusty(but not very"
+                " effective)dagger.")
 
 
 intro()
